8596b96ed791da9802d9d68d52bd04c60f6fc47c
adtm <tomas.eglinskas@gmail.com>
2017-10-02 00:42:30 +0300

    isUnique with CTCI boolean array solution

--- src/Arrays_and_Strings/IsUnique.java
+++ src/Arrays_and_Strings/IsUnique.java
@@ -5,11 +5,16 @@ import java.util.Map;
 
 public class IsUnique {
     public static void main(String[] args) {
-        String word = "aabcdef"; // return false
-        Boolean answer = isUnique(word);
+        String word = "abcdef"; // return false
+        Boolean answer = isUniqueBooleanArray(word);
         System.out.println(answer);
     }
-    
+
+    /**
+     * Hash Map solution
+     * @param word
+     * @return
+     */
     private static boolean isUnique(String word) {
         Map<Character, Integer> letterCount = new HashMap<>();
         for (char letter: word.toCharArray()) {
@@ -21,4 +26,20 @@ public class IsUnique {
         }
         return true;
     }
+
+    /**
+     * CTCI Book solution
+     * @param word
+     * @return
+     */
+    private static boolean isUniqueBooleanArray(String word) {
+        boolean[] values = new boolean[128];
+        for(int i = 0; i < word.length(); i++) {
+            int val = word.charAt(i);
+            if (values[val] == true) return false;
+            values[val] = true;
+        }
+        return true;
+    }
+
 }
