50eec095a4232486155d05e0a51b2fbd06eee9a5
adtm <tomas.eglinskas@gmail.com>
2017-11-01 23:29:57 +0200

    Repush

--- src/Arrays_and_Strings/PalindromePermutations.java
+++ src/Arrays_and_Strings/PalindromePermutations.java
@@ -1,4 +1,34 @@
 package Arrays_and_Strings;
 
+import java.util.HashMap;
+
 public class PalindromePermutations {
+    public static void main(String[] args) {
+        checkIfCanBePalindrome("Tacto Coa");
+    }
+
+    public static boolean checkIfCanBePalindrome(String word) {
+
+        int uniqueLetters = 0, foundPairs = 0;
+
+        HashMap hashMap = new HashMap<String, Integer>();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = Character.toLowerCase(word.charAt(i));
+            if (letter != ' ') {
+                if (hashMap.containsKey(letter)) {
+                    foundPairs++;
+                    hashMap.remove(letter);
+                } else {
+                    uniqueLetters++;
+                    hashMap.put(letter, i);
+                }
+            }
+
+        }
+        if (uniqueLetters == foundPairs) return true;
+        return false;
+
+    }
+
+
 }
