7861227f9200dc64571b86e3925eff95a1116cfe
adtm <tomas.eglinskas@gmail.com>
2017-11-04 02:30:58 +0200

    Stack Sort added

--- src/Stacks_and_Queues/StackSort.java
+++ src/Stacks_and_Queues/StackSort.java
@@ -1,4 +1,28 @@
 package Stacks_and_Queues;
 
+import java.util.Stack;
+
 public class StackSort {
+    private Stack<Integer> sorted = new Stack<>();
+    private Stack<Integer> temp = new Stack<>();
+
+    void push(int value) {
+        if (sorted.isEmpty()) {
+            sorted.push(value);
+            return;
+        }
+        while (!sorted.isEmpty()) {
+            if (sorted.peek() > value) {
+                temp.push(sorted.pop());
+            } else {
+                break;
+            }
+        }
+        sorted.push(value);
+        while (!temp.isEmpty()) {
+            sorted.push(temp.pop());
+        }
+        System.out.println(sorted);
+    }
+
 }
