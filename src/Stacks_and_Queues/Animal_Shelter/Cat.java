6cd1c5d5dc1ad3fb59e31934c7e3e485e22ee0fc
adtm <tomas.eglinskas@gmail.com>
2017-11-04 02:53:23 +0200

    Animal Shelter

---
+++ src/Stacks_and_Queues/Animal_Shelter/Cat.java
@@ -0,0 +1,4 @@
+package Stacks_and_Queues.Animal_Shelter;
+
+public class Cat {
+}
---
+++ src/Stacks_and_Queues/Main.java
@@ -0,0 +1,13 @@
+package Stacks_and_Queues;
+
+public class Main {
+    public static void main(String[] args) {
+       StackSort stackSort = new StackSort();
+        stackSort.push(7);
+        stackSort.push(9);
+        stackSort.push(2);
+        stackSort.push(4);
+        stackSort.push(2);
+        stackSort.push(5);
+    }
+}
