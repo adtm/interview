0cf3f174f9f1e7fe52e981254214b00d6c43ce40
adtm <tomas.eglinskas@gmail.com>
2017-11-07 01:03:09 +0200

    DFS && isBalanced

--- src/Trees_and_Graphs/Tree.java
+++ src/Trees_and_Graphs/Tree.java
@@ -1,7 +1,9 @@
 package Trees_and_Graphs;
 
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Tree {
     public Node sortedArrayToBST(int[] array) {
@@ -54,6 +56,46 @@ public class Tree {
         else {
             printLevelNodes(head.left, level - 1, list);
             printLevelNodes(head.right, level - 1, list);
+        }
+    }
+
+    void dfs(Node root) {
+        Stack<Node> queue = new Stack<>();
+
+        queue.push(root);
+        while (queue.size() != 0) {
+            Node current = queue.pop();
+            System.out.println(current.val);
+            if (current.left != null) {
+                queue.push(current.left);
+            }
+            if (current.right != null) {
+                queue.push(current.right);
+            }
+        }
+    }
+
+    boolean isBalanced(Node root) {
+
+        if (root == null) return true;
+
+        int lHeight = getHeight(root.left);
+        int rHeight = getHeight(root.right);
+
+        if (Math.abs(lHeight - rHeight) > 1) return false;
+
+        else return isBalanced(root.left) && isBalanced(root.right);
+
+    }
+
+    private int getHeight(Node root) {
+        if (root == null) return 0;
+        else {
+            int lHeight = getHeight(root.left);
+            int rHeight = getHeight(root.right);
+
+            if (lHeight > rHeight) return lHeight + 1;
+            else return rHeight + 1;
         }
     }
 }
